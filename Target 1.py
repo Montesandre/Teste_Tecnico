da1285b5e8fe0207ed7822a137983ff48b24731d
André <montesandre@outlook.com>
2025-05-09 08:56:08 -0300

    Create Target 1.py

---
+++ Target 1.py
@@ -0,0 +1,13 @@
+def desafio1():
+    indice = 13
+    soma = 0
+    k = 0
+
+    while k < indice:
+        k += 1
+        soma += k
+
+    print(f"O valor da variável SOMA é: {soma}")
+
+if __name__ == "__main__":
+    desafio1()
